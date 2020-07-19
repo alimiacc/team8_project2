b1a2431346e4350c8857dfc958e781f9fc7fa977
Andrew Bartczak <bartczaka@gmail.com>
2020-07-19 09:43:22 -0500

    Updating just plot.js file

--- Project2Code/static/js/plot.js
+++ Project2Code/static/js/plot.js
@@ -4,6 +4,9 @@
 const csvFile = [ "Produced_WK_Data", "Supplied_WK_Data", "UNL_Gas_WK_Prices", "Diesel_WK_Prices", "WTI_DL_Spot_Prices" ];
 const yTraceEntry = [ 'Produced', 'Supplied', 'Price', 'Price', 'Price' ];
 const chartName = [ "US Weekly Oil Production", "US Weekly Oil Consumption", "US Weekly UNL Gasoline Prices", "US Weekly Diesel Prices", "Cushing WTI Daily Spot Prices" ];
+const chartLegend = [ "Oil Production", "Oil Consumption", "Gasoline Prices", "Diesel Prices", "Oil Spot Prices" ];
+/* Needs to change chart line colors */
+const chartColor = [ '#17BECF', '#1a53ff', '#336600', '#003366', '#cc3300' ];
 
 // Chart Selector function to update the index value from [ 0 to 4 ] depeonding on the user's chart selection provided by HTML dropdown element
 function chartSelection(index=0) {   
@@ -14,26 +17,45 @@ function chartSelection(index=0) {
             mode: "lines",
             x: unpack(rows, 'Date'),
             y: unpack(rows, yTraceEntry[index]),
-            line: {color: '#17BECF'}
+            name: chartLegend[index],
+            line: {color: chartColor[index]}
         };
 
         var data = [trace1];
 
         // To be defined via dialog box entry by the user if the time permits
-        var startDate = '2015-07-10'; /* dynamic chart start date */
-        var endDate = '2020-07-10';   /* dynamic chart end date */
+        var startDate = '1994-03-21'; /* dynamic chart start date */
+        var endDate = '2020-07-13';   /* dynamic chart end date */
 
         var layout = {
             title: chartName[index],
             xaxis: {
                 range: [startDate, endDate],
+                // Added Code begins
+                rangeselector: {buttons: [
+                    {
+                      count: 1,
+                      label: '1m',
+                      step: 'month',
+                      stepmode: 'backward'
+                    },
+                    {
+                      count: 6,
+                      label: '6m',
+                      step: 'month',
+                      stepmode: 'backward'
+                    },
+                    {step: 'all'}
+                  ]},
+                rangeslider: {range: [startDate, endDate]},
+                // Added Code ends
                 type: 'date'
             },
             yaxis: {
                 autorange: true,
                 type: 'linear'
             },
-            showlegend: false
+            showlegend: true
         };
         // Needs to use newPlot method to overwite the existing plot    
         Plotly.newPlot('plotdiv', data, layout);
